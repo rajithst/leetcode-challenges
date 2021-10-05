69dc4eab93c6f367315f70fc3ddff05dac92ffd8
rajithst <rajiththennakoonshady@gmail.com>
2021-10-05 17:13:06 +0900

    Graph-medium- number of provinces solution optimized by path compression and union by rank

--- graph/medium/number_of_provinces.py
+++ graph/medium/number_of_provinces.py
@@ -3,27 +3,35 @@ from typing import List
 
 def findCircleNum(isConnected: List[List[int]]) -> int:
     cities = len(isConnected[0])
+    provinces = cities
     if cities == 0:
         return
     root = [i for i in range(cities)]
+    rank = [1] * cities
 
     def find(i):
         if root[i] == i:
             return i
-        return find(root[i])
+        root[i] = find(root[i])
+        return root[i]
 
-    def union(i, j):
+    def union(i, j, provinces):
         root_i = find(i)
         root_j = find(j)
         if root_i != root_j:
-            root[root_i] = root_j
+            if rank[root_i] < rank[root_j]:
+                root[root_i] = root_j
+                rank[root_j] += rank[root_i]
+            else:
+                root[root_j] = root_i
+                rank[root_i] += rank[root_j]
+
+            provinces -= 1
+        return provinces
 
     for i in range(cities):
         for j in range(cities):
             if i != j and isConnected[i][j] == 1:
-                union(i, j)
+                provinces = union(i, j, provinces)
 
-    cc = set()
-    for r in range(cities):
-        cc.add(find(root[r]))
-    return len(cc)
+    return provinces
