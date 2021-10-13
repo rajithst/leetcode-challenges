0138543dcfb3227f69827597c2120823183fdc47
rajithst <rajiththennakoonshady@gmail.com>
2021-10-13 16:42:59 +0900

    Graph-medium- All paths from source to target BFS implementation

--- graph/medium/all_paths_from_source_to_target.py
+++ graph/medium/all_paths_from_source_to_target.py
@@ -22,3 +22,20 @@ class Solution:
         path = deque([0])
         dfs_helper(0, path)
         return results
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        source = 0
+        target = len(graph) -1
+        que = deque()
+        que.append([source])
+        result = []
+
+        while que:
+            cr = que.popleft()
+            if cr[-1] == target:
+                result.append(cr)
+            else:
+                for n in graph[cr[-1]]:
+                    que.append(cr+[n])
+        return result
