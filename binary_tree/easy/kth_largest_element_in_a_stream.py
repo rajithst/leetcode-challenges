0b3194fdf2b8bdf071a8a159e2ce2a5eedd81268
rajithst <rajiththennakoonshady@gmail.com>
2021-10-25 22:35:25 +0900

    Binary Tree-easy- kth largest element in a stream implementation using minheap

---
+++ binary_tree/easy/kth_largest_element_in_a_stream.py
@@ -0,0 +1,22 @@
+import heapq as heap
+from typing import List
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = nums
+        heap.heapify(self.heap)
+        while len(self.heap) > k:
+            heap.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        heap.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heap.heappop(self.heap)
+        return self.heap[0]
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
