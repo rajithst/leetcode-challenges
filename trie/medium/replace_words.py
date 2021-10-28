a6118faab50d4d2b746aa865fd1cc9bafe381f17
rajithst <rajiththennakoonshady@gmail.com>
2021-10-28 23:35:55 +0900

    Trie -Medium- replace words implementation with bugs

---
+++ trie/medium/replace_words.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class TrieNode:
+    def __init__(self, char=""):
+        self.childrens = {}
+        self.is_end = False
+        self.val = float("inf")
+        self.char = char
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        current = self.root
+        for w in word:
+            if w not in current.childrens:
+                current.childrens[w] = TrieNode(w)
+            current = current.childrens[w]
+        current.is_end = True
+
+    def search(self, word):
+        current = self.root
+        res = ""
+        for wd in word:
+            if wd not in current.childrens:
+                break
+            res += wd
+            current = current.childrens[wd]
+            if current.is_end:
+                return res
+        return word
+
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+
+        tr = Trie()
+        for d in dictionary:
+            tr.insert(d)
+        res = []
+        for wd in dictionary:
+            rest = tr.search(wd)
+            res.append(rest)
+        return " ".join(res)
