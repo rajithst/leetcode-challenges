1d134df712d60c7c2981731776b8e84cfd8a885f
rajithst <rajiththennakoonshady@gmail.com>
2021-11-03 13:07:46 +0900

    Binart Tree - Medium - path sum ii implementation using dfs

---
+++ binary_tree/medium/path_sum_ii.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def __init__(self):
+        self.results = []
+
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+
+        def dfs(node, current_path):
+            if node is None:
+                return
+
+            current_path.append(node.val)
+            if node.left is None and node.right is None:
+                if sum(current_path) == targetSum:
+                    self.results.append(current_path.copy())
+            dfs(node.left, current_path)
+            dfs(node.right, current_path)
+
+            del current_path[-1]
+
+        dfs(root, [])
+        return self.results
