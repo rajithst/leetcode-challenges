a08ef25c6103de34ae071e2123948f23acf18f9b
rajithst <rajiththennakoonshady@gmail.com>
2021-11-09 22:31:36 +0900

    Union-Find - Medium - number of provinces union find and dfs implementation

--- graph/medium/number_of_provinces.py
+++ graph/medium/number_of_provinces.py
@@ -1,37 +1,27 @@
 from typing import List
 
 
-def findCircleNum(isConnected: List[List[int]]) -> int:
-    cities = len(isConnected[0])
-    provinces = cities
-    if cities == 0:
-        return
-    root = [i for i in range(cities)]
-    rank = [1] * cities
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
 
-    def find(i):
-        if root[i] == i:
-            return i
-        root[i] = find(root[i])
-        return root[i]
+        visited = {n: False for n in range(len(isConnected))}
+        adj_list = {n: [] for n in range(len(isConnected))}
 
-    def union(i, j, provinces):
-        root_i = find(i)
-        root_j = find(j)
-        if root_i != root_j:
-            if rank[root_i] < rank[root_j]:
-                root[root_i] = root_j
-                rank[root_j] += rank[root_i]
-            else:
-                root[root_j] = root_i
-                rank[root_i] += rank[root_j]
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected)):
+                if isConnected[i][j] == 1:
+                    adj_list[i].append(j)
+                    adj_list[j].append(i)
 
-            provinces -= 1
+        def dfs(node):
+            visited[node] = True
+            for c in adj_list[node]:
+                if not visited[c]:
+                    dfs(c)
+
+        provinces = 0
+        for i in visited.keys():
+            if not visited[i]:
+                provinces += 1
+                dfs(i)
         return provinces
-
-    for i in range(cities):
-        for j in range(cities):
-            if i != j and isConnected[i][j] == 1:
-                provinces = union(i, j, provinces)
-
-    return provinces
