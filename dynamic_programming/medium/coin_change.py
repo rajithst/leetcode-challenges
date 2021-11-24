38f87482b864d89398b0fe362b470c4fa0e77df4
rajithst <rajiththennakoonshady@gmail.com>
2021-11-24 22:38:06 +0900

    DP - medium - coin change bottom-up implementation

--- dynamic_programming/medium/coin_change.py
+++ dynamic_programming/medium/coin_change.py
@@ -22,3 +22,18 @@ class Solution:
 
         res = top_down_dp(amount)
         return -1 if res == float("inf") else res
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        dp = [0] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            dp[i] = float("inf")
+            for c in coins:
+                if i - c >= 0 and dp[i - c] != float("inf"):
+                    dp[i] = min(dp[i], dp[i - c] + 1)
+
+        return -1 if dp[amount] == float("inf") else dp[amount]
