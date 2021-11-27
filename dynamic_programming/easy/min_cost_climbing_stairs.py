65a32904bc4a28cc362d020679bc2aff8ca2003b
rajithst <rajiththennakoonshady@gmail.com>
2021-11-27 14:43:26 +0900

    DP - easy - min cost climbing stairs bottom-up implementation

--- dynamic_programming/easy/min_cost_climbing_stairs.py
+++ dynamic_programming/easy/min_cost_climbing_stairs.py
@@ -15,3 +15,15 @@ class Solution:
             return memo[at]
 
         return top_down_dp(len(cost))
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = 0
+
+        for i in range(2, n + 1):
+            dp[i] = min(cost[i - 1] + dp[i - 1], cost[i - 2] + dp[i - 2])
+        return dp[n]
