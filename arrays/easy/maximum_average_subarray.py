182821b083305180c2496edd3916dae8e6f7c8e4
rajithst <rajiththennakoonshady@gmail.com>
2021-11-30 21:38:57 +0900

    Array - easy - maximum average subarray implementation using sliding window

---
+++ arrays/easy/maximum_average_subarray.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        p1 = 0
+        p2 = k - 1
+        n = len(nums)
+        mv = cs = sum(nums[p1:p2 + 1])
+        for i in range(p2 + 1, n):
+            cs += nums[i]
+            cs -= nums[i - k]
+            mv = max(mv, cs)
+        return mv / k
