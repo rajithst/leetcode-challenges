44be7104c6d294b68fc3b91e083187bc8893d84e
rajithst <rajiththennakoonshady@gmail.com>
2021-12-01 21:44:09 +0900

    Array - medium - product of array except self implementation using right and left index array

---
+++ arrays/medium/productt_of_array_except_self.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        left = [1]*n
+        for i in range(1,n):
+            left[i] = left[i-1]*nums[i-1]
+        right = [1]*n
+        for i in range(n-2,-1,-1):
+            right[i] = right[i+1]*nums[i+1]
+        res = [0]*n
+        for i in range(n):
+            res[i] = left[i]*right[i]
+        return res
