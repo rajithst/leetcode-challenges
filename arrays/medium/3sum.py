4888d27e0a4041abfa236c1152b32d5a6ee6cc8e
rajithst <rajiththennakoonshady@gmail.com>
2022-01-30 20:09:47 +0900

    Arrays - medium - 3sum python implementation

--- arrays/medium/3sum.py
+++ arrays/medium/3sum.py
@@ -13,16 +13,16 @@ class Solution:
             diff = 0 - current_num
             p1 = i + 1
             p2 = len(nums) - 1
-            while (p1 < p2):
+            while p1 < p2:
                 r1 = sorted_nums[p1]
                 r2 = sorted_nums[p2]
                 if diff == r1 + r2:
                     results.append([current_num, r1, r2])
                     p1 += 1
                     p2 -= 1
-                    while (p1 < p2 and sorted_nums[p1] == sorted_nums[p1 - 1]):
+                    while p1 < p2 and sorted_nums[p1] == sorted_nums[p1 - 1]:
                         p1 += 1
-                    while (p1 < p2 and sorted_nums[p2] == sorted_nums[p2 + 1]):
+                    while p1 < p2 and sorted_nums[p2] == sorted_nums[p2 + 1]:
                         p2 -= 1
                 elif r1 + r2 < diff:
                     p1 += 1
