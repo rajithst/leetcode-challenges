aad066fe17d0fbb8556e6fa6ae3d92a2384482c7
rajithst <rajiththennakoonshady@gmail.com>
2022-02-21 21:56:40 +0900

    Linked list - medium - detect linked list cycle update

--- linked-list/easy/detect-linked-list-cycle.py
+++ linked-list/easy/detect-linked-list-cycle.py
@@ -3,22 +3,22 @@ class ListNode:
         self.val = x
         self.next = None
 
+
 # using the slow pointer and fast pointer method
 def has_cycle(head: ListNode) -> bool:
-
     # make slow and fast pointer to head node
     slow_pointer, fast_pointer = head, head
 
-    #if fast pointer and fast ponter next is not null (if it is null there is a end)
+    # if fast pointer and fast pointer next is not null (if it is null there is a end)
     while fast_pointer and fast_pointer.next:
 
-        #point slow pointer by one
+        # move slow pointer by one
         slow_pointer = slow_pointer.next
 
-        #point fast pointer by two
+        # move fast pointer by two
         fast_pointer = fast_pointer.next.next
 
-        #if there is a cycle eventually fast pointer will meet the slow pointer inside the cycle
+        # if there is a cycle eventually fast pointer will meet the slow pointer inside the cycle
         if slow_pointer == fast_pointer:
             return True
     return False
