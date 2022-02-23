1f18b797e75a9f52939d81a597baa5a765ae86fc
rajithst <rajiththennakoonshady@gmail.com>
2022-02-23 21:36:11 +0900

    Linked list - easy - middle of the linked list implementation using fast and slow pointer

--- linked-list/easy/middle_of_the_linked_list.py
+++ linked-list/easy/middle_of_the_linked_list.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -15,3 +18,23 @@ def middle_node(head: ListNode) -> ListNode:
     for i in range(mid):
         head = head.next
     return head
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        We can use the Fast & Slow pointers method such that the fast pointer is
+        always twice the nodes ahead of the slow pointer.
+        This way, when the fast pointer reaches the end of the LinkedList,
+        the slow pointer will be pointing at the middle node.
+        """
+        fast, slow = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
